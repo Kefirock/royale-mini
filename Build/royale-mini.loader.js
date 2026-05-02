a98562b6cefb4c27a77103dfdd490ede3f54e289
KefiRocK <id.justevolution@gmail.com>
2026-05-02 19:26:34 +0300

    fix: lobby fix v4

--- Build/royale-mini.loader.js
+++ Build/royale-mini.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 75359690,
+    wasmFileSize: 75359879,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
