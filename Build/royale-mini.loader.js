e77f863273acce2030997703b2d036644e3eb6c8
KefiRocK <id.justevolution@gmail.com>
2026-05-02 20:52:41 +0300

    upd: appear FX

--- Build/royale-mini.loader.js
+++ Build/royale-mini.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 75360217,
+    wasmFileSize: 75362083,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
