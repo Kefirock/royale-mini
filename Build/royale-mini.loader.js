9ea4c012bcd79e34c242814ba1122677cc652ce2
KefiRocK <id.justevolution@gmail.com>
2026-05-02 17:17:13 +0300

    upd: matchmaking lobby fix v2

--- Build/royale-mini.loader.js
+++ Build/royale-mini.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 75344176,
+    wasmFileSize: 75344528,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
